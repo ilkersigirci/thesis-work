0678d593cf33bca53a2d7ddccff5e7f703ac2db1
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2023-08-18 11:29:49 +0300

    finetune github test fixed

--- thesis_work/chemberta/finetune.py
+++ thesis_work/chemberta/finetune.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 import wandb
-from thesis_work.chemberta.utils import (  # noqa: F401
+from thesis_work.chemberta.utils import (
     evaluate_model,
     get_model,
     predict_model,
@@ -21,21 +21,29 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 def main():
     wandb.login()
 
+    method = "finetune"
+    # method = "evaluate"
+    # method = "predict"
+
     protein_type = "kinase"
+    # protein_type = "gpcr"
+    # protein_type = "protease"
+
     output_dir = f"{protein_type.upper()}_77M_MLM_Shuffle_80_10_10_epoch10"
 
     train_df, valid_df, test_df = load_protein_family_splits(protein_type=protein_type)
     model = get_model()
 
-    # print(model.tokenizer)
+    if method == "finetune":
+        train_model(
+            model=model, train_df=train_df, valid_df=valid_df, output_dir=output_dir
+        )
 
-    train_model(
-        model=model, train_df=train_df, valid_df=valid_df, output_dir=output_dir
-    )
-    # evaluate_model(model=model, test_df=test_df)
+    elif method == "evaluate":
+        evaluate_model(model=model, test_df=test_df)
 
-    # Lets input a molecule with a toxicity value of 1
-    # mol = "C1=C(C(=O)NC(=O)N1)F"
-    # mol = "Nc1nc(NC2CC2)c2ncn([C@H]3C=C[C@@H](CO)C3)c2n1"
+    elif method == "predict":
+        mol = "C1=C(C(=O)NC(=O)N1)F"
+        mol = "Nc1nc(NC2CC2)c2ncn([C@H]3C=C[C@@H](CO)C3)c2n1"
 
-    # predict_model(model=model, smiles_mol_list=[mol])
+        predict_model(model=model, smiles_mol_list=[mol])
